13ff8f2479b7fee2fafe6429ecefb6bfc507a1af
santhoshi kumari <santhoshi77.kotyada@gmail.com>
2024-12-18 16:59:23 +0530

    added initial files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Pushpa 2</h1>
+    <h1>Pushpa 1</h1>
+    </div>
+  );
+}
+
+export default App;
