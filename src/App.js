c93efdc30a419b083abe121b50ede59758660688
santhoshikumari7 <santhoshi77.kotyada@gmail.com>
2024-12-19 14:04:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
     <div className="App">
     <h1>Pushpa 2</h1>
     <h1>Pushpa 1</h1>
+
+    <h2>Allu Arjun</h2>
+    <img src="./alluarjun/alluarjunposter.jpg"></img>
+    <img src="./rashmika/rashmikaposter.jpg"></img>
+    <h1>This line is by me</h1>
     </div>
   );
 }
